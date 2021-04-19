13a8b90828191f319135b8e54ca33c39b1654eec
Carolina Langfeldt <carolina.langfeldt@itajai.sc.gov.br>
2021-04-19 09:35:20 -0300

    Project Config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,4 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
-  singleQuote: true,
-  trailingComma: 'all',
-  arrowParens: 'avoid',
-};
+  "singleQuote": true,
+  "trailingComma": "all"
+}
